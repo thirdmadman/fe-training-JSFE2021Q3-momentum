42ee6ea780096be901b3b4819505491de0bb3d0d
thirdmadman <thirdmadman@gmail.com>
2021-10-27 20:12:53 +0400

    feat: impl simple DayQuote class, no API

--- momentum/src/js/MomentumCore.js
+++ momentum/src/js/MomentumCore.js
@@ -1,6 +1,9 @@
 const MomentumClock = require("./MomentumClock.js");
 const BackgroundProvider = require("./BackgroundProvider.js");
 const MusicPlayer = require("./MusicPlayer.js");
+const DayQuote = require("./DayQuote.js");
+
+const defaultQuotes = require("./defaultQuotes.json");
 class MomentumCore {
   constructor(className) {
     this.className = className;
@@ -8,6 +11,11 @@ class MomentumCore {
     this.clock = new MomentumClock(this.className);
     this.backgroundProvider = new BackgroundProvider();
     this.musicPlayer = new MusicPlayer(this.className);
+    this.dayQuote = new DayQuote(this.className);
+    if (defaultQuotes) {
+      this.dayQuote.setQuotesArray(defaultQuotes);
+    }
+
   }
 
   addPlaylist(songsArray) {
